a6204d2e74e101324dee24a9af90f71b2b40bb68
3A732080 <tiffany89031176@gmail.com>
2020-10-28 11:02:39 +0800

    修改

--- public/index.php
+++ public/index.php
@@ -1,36 +1,33 @@
 <?php
-require '../vendor/autoload.php';
+require "../vendor/autoload.php";
 
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
-
-
-$world = new World();
-$lara= new Lara();
-$vincent= new Hello\Someone('Vincent');
-// 以下Someone類別的使用可以不用use Demo\Hello;
-$mary= new \Demo\Hello\Someone('Mary');
-$john= new Demo\Hello\Someone('John');
-
-echo '<br>';
-//monolog測試程式碼
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Carbon\Carbon;
+
+$world= new World();
+$lara=new Lara();
+$vincent=new Hello\Someone('Vincent');
+$mary=new Hello\Someone('Mary');
+$john=new Hello\Someone('John');
+
+//Monolog
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler('../log/your.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('../log/my.log', Logger::WARNING));
 
 // add records to the log
 $log->warning('Foo');
 $log->error('Bar');
+//Monolog
 
-echo '<br>';
-//carbon測試程式碼
-use Carbon\Carbon;
-
-printf("Right now is %s <br>", Carbon::now()->toDateTimeString());
-printf("Right now in Vancouver is %s <br>", Carbon::now('America/Vancouver'));  //implicit __toString()
+//Carbon
+printf("Now: %s<br>", Carbon::now(+8));
+printf("Right now is %s<br>", Carbon::now()->toDateTimeString());
+printf("Right now in Vancouver is %s<br>", Carbon::now('America/Vancouver'));  //implicit __toString()
 $tomorrow = Carbon::now()->addDay();
 $lastWeek = Carbon::now()->subWeek();
 $nextSummerOlympics = Carbon::createFromDate(2016)->addYears(4);
@@ -42,8 +39,6 @@ $howOldAmI = Carbon::createFromDate(1975, 5, 21)->age;
 $noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Europe/London');
 
 $internetWillBlowUpOn = Carbon::create(2038, 01, 19, 3, 14, 7, 'GMT');
-
-// Don't really want this to happen so mock now
 Carbon::setTestNow(Carbon::createFromDate(2000, 1, 1));
 
 // comparisons are always done in UTC
@@ -55,15 +50,16 @@ if (Carbon::now()->gte($internetWillBlowUpOn)) {
 Carbon::setTestNow();
 
 if (Carbon::now()->isWeekend()) {
-    echo 'Party!';
+    echo 'Party!<br>';
 }
 // Over 200 languages (and over 500 regional variants) supported:
-echo Carbon::now()->subMinutes(2)->diffForHumans()."<br>"; // '2 minutes ago'
-echo Carbon::now()->subMinutes(2)->locale('zh_TW')->diffForHumans()."<br>"; // '2分钟前'
-echo Carbon::parse('2019-07-23 14:51')->isoFormat('LLLL')."<br>"; // 'Tuesday, July 23, 2019 2:51 PM'
+echo Carbon::now()->subMinutes(2)->diffForHumans().'<br>'; // '2 minutes ago'
+echo Carbon::now()->subMinutes(2)->locale('zh_TW')->diffForHumans().'<br>'; // '2分鐘前'
+echo Carbon::parse('2019-07-23 14:51')->isoFormat('LLLL').'<br>'; // 'Tuesday, July 23, 2019 2:51 PM'
 echo Carbon::parse('2019-07-23 14:51')->locale('fr_FR')->isoFormat('LLLL'); // 'mardi 23 juillet 2019 14:51'
 
 // ... but also does 'from now', 'after' and 'before'
 // rolling up to seconds, minutes, hours, days, months, years
 
 $daysSinceEpoch = Carbon::createFromTimestamp(0)->diffInDays();
+//Carbon
